2c2344efce2c478bb80a04fc7052c5a324a13282
Aziz-Taufique <taufiqueaziz88@gmail.com>
2026-02-14 23:07:30 +0530

    main

--- recursion/03_recursion_with_static_variable.cpp
+++ recursion/03_recursion_with_static_variable.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-static int x = 0;
+// static variable has single copy.
+// int x = 0; // it will also has single copy
 
 int fn(int n)
 {
+    static int x = 0;
     if (n > 0)
     {
         x++;
